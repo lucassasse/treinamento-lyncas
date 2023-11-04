ce7ba2ab5518013751bfe0a9fce63256d68d8193
Lucas Eduardo Sasse <lucas1sasse@gmail.com>
2023-11-04 11:51:29 -0300

    bug fixed

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -23,7 +23,7 @@ export default {
   methods:{
     insertItemOnList(){
       this.items.push({
-        id:this.items.length + 1, 
+        id:this.items.length + 1,
         important:this.important,
         checked: false,
         label: this.newItem
@@ -35,8 +35,8 @@ export default {
 
     },
     checkItem(){
-      this.item.checked !== "checked" 
-      ? this.item.checked = "checked" 
+      this.item.checked !== "checked"
+      ? this.item.checked = "checked"
       : this.item.checked = ""
     },
     checkAllItem(){
@@ -53,60 +53,70 @@ export default {
 </script>
 
 <template>
-	<h1>{{ header || "Welcome" }}</h1>
-  <input v-model="newItem" @keypress.enter="insertItemOnList" placeholder="Item...">
-  <br>
-  <input type="checkbox" v-model="important"> Important
-  <br>
-  <button @click="insertItemOnList">Save</button>
-  <br><br><hr>
-  
-  <div id="importants">
-    <h3>Importants:</h3>
-    <ul v-if="items.length > 0" >
-      <template v-for="item in filteredCheckedItems" :key="item.id">
-        <li v-if="item.important">
-          <input type="checkbox" v-model="item.checked">
-          <span :class="{ checked: item.checked }">
-            {{item.label}} - {{item.important}} - {{ item.checked }}
-          </span>
-          <button>Edit</button>
-          <button @click="removeItem(item)">X</button>
-        </li>
-      </template>
-    </ul>
-    <p v-else>No items on the list!</p>
-  </div>
+	<div id="main">
+		<h1>{{ header || "Welcome" }}</h1>
+		<input v-model="newItem" @keypress.enter="insertItemOnList" placeholder="Item..."> <br>
+		<input type="checkbox" v-model="important"> Important <br>
+		<button @click="insertItemOnList">Save</button> <br><br><hr>
 
-  <hr>
+		<div id="importants">
+			<h3>Importants:</h3>
+			<ul v-if="items.length > 0" >
+				<template v-for="item in filteredCheckedItems">
+					<li v-if="item.important" :key="item.id">
+						<input type="checkbox" v-model="item.checked">
+						<span :class="{ checked: item.checked }">
+							{{item.label}} - {{item.important}} - {{ item.checked }}
+						</span>
+						<button>Edit</button>
+						<button @click="removeItem(item)">X</button>
+					</li>
+				</template>
+			</ul>
+			<p v-else>No items on the list!</p>
+		</div>
+		<hr>
 
-  <div id="notImportants">
-    <h3>Not Importants:</h3>
-     <ul v-if="items.length > 0" >
-      <template v-for="item in filteredCheckedItems" :key="item.id">
-        <li v-if="!item.important">
-          <input type="checkbox" v-model="item.checked">
-          <span :class="{ checked: item.checked }">
-            {{item.label}} - {{item.important}} - {{ item.checked }}
-          </span>
-          <button>Edit</button>
-          <button @click="removeItem(item)">X</button>
-        </li>
-      </template>
-    </ul>
-    <p v-else>No items on the list!</p>
-  </div>
+		<div id="notImportants">
+			<h3>Not Importants:</h3>
+			<ul v-if="items.length > 0" >
+				<template v-for="item in filteredCheckedItems">
+					<li v-if="!item.important" :key="item.id">
+						<input type="checkbox" v-model="item.checked">
+						<span :class="{ checked: item.checked }">
+							{{item.label}} - {{item.important}} - {{ item.checked }}
+						</span>
+						<button>Edit</button>
+						<button @click="removeItem(item)">X</button>
+					</li>
+				</template>
+			</ul>
+			<p v-else>No items on the list!</p>
+		</div>
+		<hr>
 
-  <hr>
-  <button @click="checkAllItem">Check all items</button> <br>
-  <button @click="removeAllItem">Remove all items</button> <br>
-  <button @click="hideCheckedItems = !hideCheckedItems">
-    {{ hideCheckedItems ? "Show all items" : "Hide checked items"}}
-  </button>
+		<button @click="checkAllItem">Check all items</button> <br>
+		<button @click="removeAllItem">Remove all items</button> <br>
+		<button @click="hideCheckedItems = !hideCheckedItems">
+			{{ hideCheckedItems ? "Show all items" : "Hide checked items"}}
+		</button>
+	</div>
 </template>
 
 <style scoped>
 .checked{
   text-decoration: line-through;
+}
+#main{
+	font-family: Arial, Helvetica, sans-serif;
+	width: 350px;
+	margin-left: auto;
+    margin-right: auto;
+	padding: 25px;
+	border: 1px solid #ccc;
+	border-radius: 5px;
+}
+li{
+	list-style: none;
 }
 </style>
