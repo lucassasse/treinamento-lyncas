e1a29cb90da18ad5663831c4fcb70c3b3fd890d4
unknown <lyncas.es@lyncas.net>
2023-11-07 13:42:02 -0300

    criacao de api buscador de cep + alteracao nome pasta todo list

---
+++ vuejs-cep-api/src/App.vue
@@ -0,0 +1,117 @@
+<template>
+  <div id="main">
+    <div id="top">
+      <h1>
+        CEP API
+      </h1>
+      <p>Informe o CEP</p>
+      <input maxlength="9" placeholder="12345-678" v-model="cep"/>
+    </div>
+
+    <div id="bottom" v-if="response !== null">
+      <p>Estado</p>
+      <input disabled :value="estado"/>
+      <p>Cidade</p>
+      <input disabled :value="cidade"/>
+      <p>Bairro</p>
+      <input disabled :value="bairro"/>
+      <p>Rua</p>
+      <input disabled :value="logradouro"/>
+      <p>Número</p>
+      <input/>
+      <p>Complemento</p>
+      <input/> <br>
+      <button>Enviar</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+  export default{
+    data(){
+      return{
+        cep: '',
+        baseUrl: 'https://viacep.com.br/ws/',
+        response: null,
+        estado: '',
+        cidade: '',
+        bairro: '',
+        logradouro: ''
+      }
+    },
+    methods:{
+       getCep(){
+        const url = `${this.baseUrl}${this.cep}/json/`
+        axios.get(url).then(resp =>{
+          const data = resp.data
+          if(!data.erro){
+            this.response = data
+            this.estado = data.uf
+            this.cidade = data.localidade
+            this.bairro = data.bairro
+            this.logradouro = data.logradouro
+          } else{
+            alert("CEP não encontrado!")
+          }
+        }).catch(error => {
+          console.log(error)
+        })
+      },
+      cepMask(data){
+        console.log(data)
+        let cep = data.toString().replace(/\D/g,'')
+        cep = data.toString().replace(/(\d{5})(\d)/,'$1-$2')
+        return cep
+      }
+    },
+      watch: {
+        cep: function (newCep) {
+          this.cep = newCep.replace("-", "")
+          if (newCep.length === 8) this.getCep()
+          else this.response = null
+        }
+      }
+
+  }
+</script>
+
+<style>
+#main {
+  font-family: Arial, Helvetica, sans-serif;
+
+  display: flex;
+  flex-direction: column; 
+  align-items: center;
+  justify-content: center;
+}
+
+#top{
+  margin: 25px 0;
+  padding: 15px;
+  border: 1px solid gray;
+  border-radius: 5px;
+}
+
+#bottom{
+  padding: 15px;
+  border: 1px solid gray;
+  border-radius: 5px;
+}
+
+h1{
+  margin-top: 0;
+}
+
+p{
+  margin-bottom: 0px;
+}
+
+input{
+  display: block;
+  height: 22px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 4px 10px;
+}
+</style>
