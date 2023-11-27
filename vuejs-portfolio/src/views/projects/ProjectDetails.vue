2f9463797ab9c755751c248297dbc4097e907d4c
unknown <lucas1sasse@gmail.com>
2023-11-27 11:04:28 -0300

    bug fixed

--- vuejs-portfolio/src/views/projects/ProjectDetails.vue
+++ vuejs-portfolio/src/views/projects/ProjectDetails.vue
@@ -1,14 +1,13 @@
 import { routeLocationKey } from 'vue-router';
 <template>
-    <h1>Project Details - {{ name }} - Page</h1>
+    <h1>Project Details - NAME - Page</h1>
     <p>The project id is {{ id }}</p>
 </template>
 
 <script>
 export default {
     props: {
-    id: { type: String },
-    name: { type: String }
+    id: { type: String }
   },
 }
 </script>
