5995cd8519d255c92338c7f240c44e3b36a06c9c
lucassasse <lucas1sasse@gmail.com>
2024-05-20 23:16:56 -0300

    create a new server project

---
+++ ServerWithMediatorAndCQRS/ServerCQRS.Domain/Abstractions/IMemberRepository.cs
@@ -0,0 +1,13 @@
+﻿using ServerCQRS.Domain.Entities;
+
+namespace ServerCQRS.Domain.Abstractions
+{
+    public interface IMemberRepository
+    {
+        Task<IEnumerable<Member>> GetAll();
+        Task<Member> GetMemberById(int memberId);
+        Task<Member> AddMember(Member member);
+        Task<Member> UpdateMember(Member member);
+        Task DeleteMember(int memberId);
+    }
+}
